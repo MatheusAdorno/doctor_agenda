3f049e33a01837168db9c0297a7a59c90d667b67
Matheus Adorno <matheusadorno11@gmail.com>
2023-07-06 16:22:34 -0300

    cors

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   source: '/src/pages/api/:path*',
   headers: [
     { key: 'Access-Control-Allow-Credentials', value: 'true' },
-    { key: 'Access-Control-Allow-Origin', value: '*' },
+    { key: 'Access-Control-Allow-Origin', value: 'https://doctoragenda.app' },
     {
       key: 'Access-Control-Allow-Methods',
       value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
