c51f54f694ec1f67c0c1c623c0ef6cfbe744703c
Matheus Adorno <matheusadorno11@gmail.com>
2023-07-06 15:56:35 -0300

    cors

--- next.config.js
+++ next.config.js
@@ -6,10 +6,14 @@ const nextConfig = {
   images: {
     domains: ['https://lh3.googleusercontent.com/'],
   },
+  source: '/api/:path*',
   headers: [
     { key: 'Access-Control-Allow-Credentials', value: 'true' },
-    { key: 'Access-Control-Allow-Origin', value: '*' }, // replace this your actual origin
-    { key: 'Access-Control-Allow-Methods', value: 'GET,DELETE,PATCH,POST,PUT' },
+    { key: 'Access-Control-Allow-Origin', value: '*' },
+    {
+      key: 'Access-Control-Allow-Methods',
+      value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
+    },
     {
       key: 'Access-Control-Allow-Headers',
       value:
